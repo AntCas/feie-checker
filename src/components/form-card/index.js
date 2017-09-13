31f5cbbf044e31defd890c298e5da3fcb55cb09c
Anthony Castrio <anthonycastrio@gmail.com>
2017-09-12 23:18:20 -0700

    show form labels

--- src/components/form-card/index.js
+++ src/components/form-card/index.js
@@ -1,11 +1,30 @@
+import _ from 'lodash';
+
 import React, { Component } from 'react';
 
 import './style.css';
 
 export default class FormCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      answer: null
+    };
+  }
+
   render() {
     const { onSubmit, content } = this.props,
           { question, helpText, answers } = content;
+
+    const labels = _.map(answers, (answer, i) =>
+      <label key={ i }>
+          { `${answer}` }
+          <input type='radio'
+            checked={ answer === this.state.answer }
+            onClick={ () => this.setState({ answer }) } />
+      </label>
+    );
 
     return (
       <div className="title-card">
@@ -14,6 +33,9 @@ export default class FormCard extends Component {
           <div className="sub-headers">
             <h2>{ `${helpText}` }</h2>
           </div>
+          <form>
+            { labels }
+          </form>
           <div className="button" onClick={ onSubmit }>
             <span>Next Question</span>
           </div>
