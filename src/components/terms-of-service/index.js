9120daac454538e74d6d264c3067c43f0ef00472
Anthony Castrio <anthonycastrio@gmail.com>
2020-01-03 13:38:09 -0500

    fix wrong link in terms

--- src/components/terms-of-service/index.js
+++ src/components/terms-of-service/index.js
@@ -13,7 +13,7 @@ export default class AnalysisCard extends Component {
 
         <h3>1. Terms</h3>
 
-          <p>By accessing the website at <a href="http://www.mysite.com">http://www.mysite.com</a>, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.</p>
+          <p>By accessing the website at <a href="http://www.feie-test.com/">http://www.feie-test.com/</a>, you are agreeing to be bound by these terms of service, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this website are protected by applicable copyright and trademark law.</p>
 
         <h3>2. Use License</h3>
 
